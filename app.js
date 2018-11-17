831d634ff76285d5cedfccd25d25df0c46302b34
Michael O'Sullivan <mjjos1@gmail.com>
2018-11-17 20:34:53 +0000

    Updated app.js to toggle the light state with each call to /toggle.

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const app = express()
 const port = 4000
 //Tell Express to serve static html files from the 'public' folder.
 app.use(express.static('public'))
+//Create a variable to store the state of the light (on or off) at any given time,
+//assume off (false) to start with
+var light_on = false;
 
-
-//Configure express to listen for HTTP GET requests at "/"
-//(i.e. http://localhost:port"/") <= No need to include the "/" at the end here.
-app.get("/", (req, res) => {
+//Configure express to listen for HTTP GET requests at "/toggle"
+//(i.e. http://localhost:port"/toggle")
+app.get("/toggle", (req, res) => {
 
     //Let the const 'spawn' hold the child_process module.
     const { spawn } = require("child_process");
-    //Call the child_process to run the Python script - store the output in pyProg.
-    const pyProg = spawn("python", ["./code/my_script.py"]);
+    var pyProg;
+
+    //Toggle the light to the opposite value of whatever it is right now
+    light_on = !light_on;
+
+    //Call the child_process to run the Python script to turn on or off the light
+    //appropriately, depending on the current value of light_on - store the output in pyProg.
+    if (light_on) {
+      pyProg = spawn("python", ["./code/script_turn_on.py"]);
+    }
+    else {
+      pyProg = spawn("python", ["./code/script_turn_off.py"]);
+    }
 
     //When output data (i.e. standard output) has been detected in 'pyProg', run
     //this function to handle that output - the variable 'data' holds the output.
